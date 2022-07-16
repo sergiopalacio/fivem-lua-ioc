27b4788783cfa93c86465b70b8b9dc46c6564680
Sergio Palacio Rodenas <sergiopalaciobach@gmail.com>
2022-07-16 13:26:13 +0200

    License

--- src/InstanceManager.lua
+++ src/InstanceManager.lua
@@ -1,3 +1,16 @@
+--Copyright 2022 Sergio Palacio Ródenas
+--
+--Licensed under the Apache License, Version 2.0 (the "License");
+--you may not use this file except in compliance with the License.
+--You may obtain a copy of the License at
+--
+--http://www.apache.org/licenses/LICENSE-2.0
+--
+--Unless required by applicable law or agreed to in writing, software
+--distributed under the License is distributed on an "AS IS" BASIS,
+--WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--See the License for the specific language governing permissions and
+--limitations under the License.
 _G.InstanceManager = {}
 
 InstanceManagerMethods = {}
@@ -33,7 +46,6 @@ InstanceManagerMethods.__index = {
     end,
     _getExistingInstanceFromId = function(self, id)
         local definition = self._definitions[id]
-        print(json.encode(definition.args))
         if definition.shared == false then
             return self:getInstanceFromDefinition(definition)
         end
@@ -73,7 +85,6 @@ InstanceManagerMethods.__index = {
 
         for _, arg in pairs(args) do
             local services = self:_resolveServices('Reference', arg)
-            print('services', json.encode(services))
             if services ~= nil then
                 table.insert(resolvedArgument, services)
             end
